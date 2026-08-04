f5cac7a754c3b0acf6e004bdfaff4baf8e95d310
aPenningTon1900 <apollonia.pennington@pcc.edu>
2026-08-04 15:12:34 -0700

    Update calorie calculation logic in Module_2.cpp
    Fix input handling and output for calorie calculation.

--- Assignment 2/Module_2.cpp
+++ Assignment 2/Module_2.cpp
@@ -1,1 +1,17 @@
+// 5 Oreo cookies make-up 1 serving and each serving has 160 calories.
+
+#include <iostream>
+using namespace std;
+int main () {
+
+int numCalories;
+int numCookies;
+int numServing;
+
+cin  >> numServing;
+numServing = 5 * numCookies;
+
+cout << 
+
+cout << “How many calories?” endl;
 
